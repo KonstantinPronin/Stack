393c5d3f963e8bd7ff03457eed2b529772691121
KonstantinPronin <prosayfer@mail.ru>
2016-12-16 12:03:51 +0300

    Update main.cpp

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,18 +1,6 @@
 #include <stack.hpp>
 
 int main() {
-	try {
-		stack<int> st;
-		st.push(1);
-		st.push(2);
-		st.push(5);
-
-		st.pop();
-		std::cout << st.top() << std::endl;
-	}
-	catch (std::logic_error& e) {
-		std::cout << e.what();
-	}
 
 	return 0;
 }
